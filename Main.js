485348eb386cc0fbde0b60802e3761e74501b65b
lolodu17 <34281237+lolodu17@users.noreply.github.com>
2018-10-17 17:03:08 +0200

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const client = new Discord.Client()
+
+const content = new Discord.Client()
+
+
+
+var prefix = ""
+bot.on("ready", function () {
+
+bot.user.setUsername("Just'Moon bot")
+
+console.log(bot.users.displayName + " est connecté");
+})
+
+
+
+
+process.env.TOKEN
+bot.login("NTAyMTEwNTc0Mjk4MDA1NTA1.DqjMpQ.RSiOdIwhA73xgsrdZalOut7Mt5U")
