c3985ff785197608098f8c94d47a7a63786b48c6
lolodu17 <34281237+lolodu17@users.noreply.github.com>
2018-10-17 17:17:26 +0200

    Add files via upload

---
+++ play.js
@@ -0,0 +1,13 @@
+const ytdl = require('ytdl-core');
+exports.run = async (client, message, args, ops) => {
+	if (!message.member.voiceChannel) return message.channel.reply("Merci de te connecté à un des vocales !")
+if (message.guild.me.voiceChannel) return message.channel.send("Désoler , le n'arrive pas à rejoindre ce vocale , essayez en un autre !")
+if (!args[0]) return message.channel.reply("Désoler , l'url à était soit incorrect , soit supprimer .")
+let validate = await ytdl.validateURL(args[0]);
+if (!validate) return message.channel.reply("Désoler , mais je ne peut pas vous lire cette vidéo :/")
+let info = await ytdl.getInfo(args[0]);
+let connection = await message.member.voiceChannel.join();
+let dispatcher = await connection.play(ytdl(args[0, { filter: 'audiooly' }]));
+message.channel.send("Vous êtes en train d'écouter : " + ${info.title});
+
+}
