9047d3ac141d88b8e4ac9f499dd491579a253b94
lolodu17 <34281237+lolodu17@users.noreply.github.com>
2018-10-17 20:16:22 +0200

    Update play.js

--- play.js
+++ play.js
@@ -1,4 +1,11 @@
 const ytdl = require('ytdl-core');
+const Discord = require("discord.js")
+const bot = New Discord.Client()
+bot.on("message", message => {
+	if (message.content.startsWith("!play")){
+	
+let xo03 = xoargs.join(" ")
+ if(!xo03) return message.reply("Désoler , vous n'avez pas mit de lien.");
 exports.run = async (client, message, args, ops) => {
 	if (!message.member.voiceChannel) return message.channel.reply("Merci de te connecté à un des vocales !")
 if (message.guild.me.voiceChannel) return message.channel.send("Désoler , le n'arrive pas à rejoindre ce vocale , essayez en un autre !")
@@ -10,4 +17,5 @@ let connection = await message.member.voiceChannel.join();
 let dispatcher = await connection.play(ytdl(args[0, { filter: 'audiooly' }]));
 message.channel.send("Vous êtes en train d'écouter : " + ${info.title});
 
-}
+}	}
+})
